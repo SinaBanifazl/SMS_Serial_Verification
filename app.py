11c00e733224ae3000ff967400b44760bbe52d28
SinaBanifazl <sinabanifazl81@gmail.com>
2025-03-26 19:43:13 +0330

    video 5 was watched

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask , jsonify , request
+import requests
+import config
 app = Flask(__name__)
 
 
@@ -9,11 +11,19 @@ def process():
     sender = data["form"]
     message = data["message"]
     print ({f"received {massage} from {processed}")
+    send_sms(sender , 'hi ' + message)
     ret = {"message" : "processed"}
     return jsonify(data) , 200
 
-def send_sms():
-    pass
+def send_sms(receptor , message):
+    '''this tabe will get a MSISDN and a message, then uses kavenegar to send sms '''
+    url = f'https://api.kavenegar.com/v1/[config.API_KEY]/sms/send.json'
+    data = {"message" : message ,
+            "receptor" : receptor}
+    r = request.post(url , data)
+    print(f"message *{message}* sent status code is {r.status_code}")
 
 def check_serial():
-    pass
+
+if __name__ == "__main__":
+    app("0.0.0.0" , 5000 , debug=True)
