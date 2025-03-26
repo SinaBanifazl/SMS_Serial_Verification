9595a067703db04c9c4443b547497eb88ee160f2
SinaBanifazl <sinabanifazl81@gmail.com>
2025-03-26 19:11:29 +0330

    video 5 dide shod . ghabli 4 bot

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask , jsonify , request
 app = Flask(__name__)
 
 
-@app.route('/')
-def main_page():
-    '''this is main of the site'''
-
-    return 'hello'
-
-@app.route('/v1/getsms')
-def get_sms():
-    pass
+@app.route('/v1/process' , methods=['post'])
+def process():
+    '''this is the call back from kavenagar . will get sender message and will check if it is valid, then answer back'''
+    data = request.form
+    sender = data["form"]
+    message = data["message"]
+    print ({f"received {massage} from {processed}")
+    ret = {"message" : "processed"}
+    return jsonify(data) , 200
 
 def send_sms():
     pass
