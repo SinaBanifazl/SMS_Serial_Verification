709bd5b0ad61df22c02f42559d0714e62467932d
SinaBanifazl <sinabanifazl81@gmail.com>
2025-03-27 21:00:33 +0330

    kol moshkelat hal shot. code kamelan tatbigh dade shod. video 13 dide shod. derim baraye apdate video 14 be baad :D

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# SMS_Serial_Verification
+# sms_verification
+
+
+## TODO
+- [x] Farhad seifi https://ngrok.com/
+- [x] add db path to config.py.sample
+- [x] do more while normalizing, specially against SQLInjection. remove all non alpha numerical
+- [ ] Atomic problem when I'm commiting every 10 inserts
+- [x] some health check url
+- [ ] get input only from KaveNegar URL 
+- [ ] there is problem with JJ1000000 and JJ100
+- [ ] create requirements.txt (pip freeze)
+- [ ] the insert will fail if there is a ' or " in excel file
+- [x] another 10 % problem :D
+- [x] refactor name str in normalize function
+- [ ] in normalize, convert AB001 to AB00001 (max len? say 15)
+- [ ] dockerize (alpine? search for uwsgi)
+- [x] merge pull requests.. check I mean :)
+
--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask , jsonify , request , Response , redirect , url_for , session , abort
-from flask_login import LoginManager , UserMixin , login_required , login_user , logout_user 
+from flask import Flask, jsonify, request, Response, redirect, url_for, session, abort
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 from pandas import read_excel
+import requests
 import re
 import sqlite3
-import requests
 import config
 
 app = Flask(__name__)
@@ -16,10 +16,7 @@ login_manager.login_view = "login"
 app.config.update(
     SECRET_KEY = config.SECRET_KEY
 )
-
-# silly user model
 class User(UserMixin):
-
     def __init__(self, id):
         self.id = id
 
@@ -27,8 +24,8 @@ class User(UserMixin):
         return "%d" % (self.id)
 
 
-# create some users with ids 1 to 20       
-users = User[0]
+# create some users with ids 1 to 20
+user = User(0)
 
 # some protected url
 @app.route('/')
@@ -36,15 +33,15 @@ users = User[0]
 def home():
     return Response("Hello World!")
 
-# somewhere to login
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
+    if request.method == 'POST': #TODO: stop the brute force
         username = request.form['username']
-        password = request.form['password']        
+        password = request.form['password']
         if password == config.PASSWORD and username == config.USERNAME:
             login_user(user)
-            return redirect('/')
+            return redirect('/') 
         else:
             return abort(401)
     else:
@@ -55,7 +52,6 @@ def login():
             <p><input type=submit value=Login>
         </form>
         ''')
-
 
 # somewhere to logout
 @app.route("/logout")
@@ -69,120 +65,156 @@ def logout():
 @app.errorhandler(401)
 def page_not_found(error):
     return Response('<p>Login failed</p>')
-    
-    
-# callback to reload the user object        
+
+
+# callback to reload the user object
 @login_manager.user_loader
 def load_user(userid):
     return User(userid)
 
+
 @app.route('/v1/ok')
 def health_check():
-    ret = {'message' : 'ok'}
+    ret = {'message': 'ok'}
     return jsonify(ret), 200
 
-@app.route('/v1/process' , methods=['post'])
-def process():
-    '''this is the call back from kavenagar . will get sender message and will check if it is valid, then answer back'''
-    data = request.form
-    sender = data["form"]
-    message = normalize_string(data["message"])
-    print ({f"received {massage} from {processed}")
-           
-    answer = check_serial(message)
+def send_sms(receptor, message):
+    """ this function will get a MSISDN and a messaage, then
+    uses KaveNegar to send sms.
+    """
+    url = f'https://api.kavenegar.com/v1/{config.API_KEY}/sms/send.json'
+    data = {"message": message,
+            "receptor": receptor}
+    res = requests.post(url, data)
+    print(f"message *{message}* sent. status code is {res.status_code}")         
 
-    send_sms(sender , answer)
-    ret = {"message" : "processed"}
-    return jsonify(data) , 200
-
-def send_sms(receptor , message):
-    '''this tabe will get a MSISDN and a message, then uses kavenegar to send sms '''
-    url = f'https://api.kavenegar.com/v1/[config.API_KEY]/sms/send.json'
-    data = {"message" : message ,
-            "receptor" : receptor}
-    r = request.post(url , data)
-    print(f"message *{message}* sent status code is {r.status_code}")
-
-def normalize_string(data):
-    persian_numerals = '۱۲۳۴۵۶۷۸۹۰'
-    arabic_numerals = '١٢٣٤٥٦٧٨٩٠'
-    english_numerals = '1234567890'
-    for i in range(len(fron_char))
-        data = data.replace(from_char[i] , to_char[i])
+def normalize_string(data, fixed_size=30):
+    from_persian_char = '۱۲۳۴۵۶۷۸۹۰'
+    from_arabic_char = '١٢٣٤٥٦٧٨٩٠'
+    to_char = '1234567890'
+    for i in range(len(to_char)):
+        data = data.replace(from_persian_char[i], to_char[i])
+        data = data.replace(from_arabic_char[i], to_char[i])
     data = data.upper()
-    data = re.sub(r'\W+' , '' , data)
+    data = re.sub(r'\W+', '', data)  # remove any non alphanumeric character
+    all_alpha = ''
+    all_digit = ''
+    for c in data:
+        if c.isalpha():
+            all_alpha += c
+        elif c.isdigit():
+            all_digit += c
+
+    missing_zeros = fixed_size - len(all_alpha) - len(all_digit)
+
+    data = all_alpha + '0' * missing_zeros + all_digit    
+
     return data
 
-def import_databace_from_exel:
-    '''gets an exel file name and imports lookup data (data and failures) from it'''
-    #def contains lookap data in the form of ---
-    df = read_excel(filepasth , 0)
-    serial_counter = 0
-    for index , (line, ref, dest, start_serial, end_serial, date) in df.interrous():
-        query = f'INSERT INTO serials VALUES ("{line}", "{ref}", "{dest}", "{start_serial}", "{end_serial}", "{date}")
-        cue.execute(query)
-        if serials_counter % 10 == 0:
-           conn.commit()
-        serial_counter += 1
-    conn.commit()
+def import_database_from_excel(filepath):
+    """ gets an excel file name and imports lookup data (data and failures) from it
+    the first (0) sheet contains serial data like:
+     Row	Reference Number	Description	Start Serial	End Serial	Date
+    and the 2nd (1) contains a column of invalid serials. 
 
-    conn = sqlite3.connect(config.DATABACE_FILE_PATH)
+    This data will be writeen into the sqlite database located at config.DATABASE_FILE_PATH
+    in two tables. "serials" and "invalids"
+
+    returns two integers: (number of serial rows, number of invalid rows)
+    """
+    # df contains lookup data in the form of 
+    # Row	Reference Number	Description	Start Serial	End Serial	Date
+
+    # TODO: make sure that the data is imported correctly, we need to backup the old one
+    # TODO: do some normaliziation
+
+    ## our sqlite database wil contain two tables: serials and invalids
+    conn = sqlite3.connect(config.DATABASE_FILE_PATH)
     cur = conn.cursor()
 
+    # remove the serials table if exists, then craete the new one
     cur.execute('DROP TABLE IF EXISTS serials')
-    cur.execute("""CREATE TABLE IF NOT EXISTS people (
+    cur.execute("""CREATE TABLE IF NOT EXISTS serials (
         id INTEGER PRIMARY KEY,
         ref TEXT,
         desc TEXT,
-        start serial TEXT,
-        end serial TEXT,
+        start_serial TEXT,
+        end_serial TEXT,
         date DATE);""")
     conn.commit()
 
-    df = read_excel(filepath , 1) #sheet one contains failed serial numbers , only colums
-    
-    for index , (failed_serial_row) in df.iterrows():
-        failed_serial = failed_serial_row[0]
-        print(failed_serial)
-
-    cur.execute('DROP TABLE IF EXISTS serials')
-    cur.execute("""CREATE TABLE IF NOT EXISTS people (
-        ref TEXT,
-        desc TEXT,
-        start serial TEXT,
-        end serial TEXT,
-        date DATE);""")
-    conn.commit()
-
-    invalid_counter = 0
-    for index , (failed_serial_row) in df.iterrows():
+    df = read_excel(filepath, 0) 
+    serials_counter = 0
+    for index, (line, ref, desc, start_serial, end_serial, date) in df.iterrows():
         start_serial = normalize_string(start_serial)
         end_serial = normalize_string(end_serial)
-        query = f'INSERT INTO serials VALUES ("{faid serial}")
-        cue.execute(query)
+        query = f'INSERT INTO serials VALUES ("{line}", "{ref}", "{desc}", "{start_serial}", "{end_serial}", "{date}");'
+        cur.execute(query)
+        # TODO: do some more error handling
+        if serials_counter % 10 == 0:
+            conn.commit()
+        serials_counter += 1
+    conn.commit()
+
+    # now lets save the invalid serials.
+
+    # remove the invalid table if exists, then craete the new one
+    cur.execute('DROP TABLE IF EXISTS invalids')
+    cur.execute("""CREATE TABLE IF NOT EXISTS invalids (
+        invalid_serial TEXT PRIMARY KEY);""")
+    conn.commit()
+    invalid_counter = 0
+    df = read_excel(filepath, 1) 
+    for index, (failed_serial, ) in df.iterrows():
+        query = f'INSERT INTO invalids VALUES ("{failed_serial}")'
+        cur.execute(query)
+        # TODO: do some more error handling
         if invalid_counter % 10 == 0:
-           conn.commit()
+            conn.commit()
         invalid_counter += 1
     conn.commit()
+
     conn.close()
 
-def check_serial():
-    '''this tabe will get one serial number and return approporiate answer to that, after consulting the db'''
-    conn = sqlite3.connect(config.DATABACE_FILE_PATH)
+    return (serials_counter, invalid_counter)
+
+def check_serial(serial):
+    """ this function will get one serial number and return appropriate
+    answer to that, after consulting the db
+    """
+    conn = sqlite3.connect(config.DATABASE_FILE_PATH)
     cur = conn.cursor()
 
     query = f"SELECT * FROM invalids WHERE invalid_serial == '{serial}'"
     results = cur.execute(query)
-    if len(requests.fetchall()) == 1:
-        return 'this serial is among failed ones'
+    if len(results.fetchall()) == 1:
+        return 'this serial is among failed ones' #TODO: return the string provided by the customer
 
-    query = f"select * FROME serials WHERE start_serial  < '{serial}' and
-        end_serial > '{serial}'"
-    results = cir.execute(query)
-    if len(results.derchall()) == 1:
-        return 'I found your serial'
+    query = f"SELECT * FROM serials WHERE start_serial < '{serial}' and end_serial > '{serial}';"
+    results = cur.execute(query)
+    if len(results.fetchall()) == 1:
+        return 'I found your serial' # TODO: return the string provided by the customer
 
     return 'it was not in the db'
 
+
+@app.route('/v1/process', methods=['POST'])
+def process():
+    """ this is a call back from KaveNegar. Will get sender and message and
+    will check if it is valid, then answers back
+    """
+    data = request.form
+    sender = data["from"]
+    message = normalize_string(data["message"])
+    print(f'received {message} from {sender}') #TODO: logging
+
+    answer = check_serial(message)
+
+    send_sms(sender, answer)
+    ret = {"message": "processed"}
+    return jsonify(ret), 200
+
 if __name__ == "__main__":
-    app("0.0.0.0" , 5000 , debug=True)
+    import_database_from_excel('../data.xlsx') 
+    app.run("0.0.0.0", 5000, debug=True)
+    
