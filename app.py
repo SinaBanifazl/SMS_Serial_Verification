481c5b30bc12320650f57dfd87a213bebe1c5d59
SinaBanifazl <sinabanifazl81@gmail.com>
2025-03-27 15:36:57 +0330

    video 8 was watched

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask , jsonify , request
 from pandas import read_excel
+import sqlite3
 import requests
 import config
 app = Flask(__name__)
@@ -28,14 +29,52 @@ def import_databace_from_exel:
     '''gets an exel file name and imports lookup data (data and failures) from it'''
     #def contains lookap data in the form of ---
     df = read_excel(filepasth , 0)
-    for index , (line, ref, dest, start_serial, end_serial, date)
-        print(line, ref, dest, start_serial, end_serial, date)
+    serial_counter = 0
+    for index , (line, ref, dest, start_serial, end_serial, date) in df.interrous():
+        query = f'INSERT INTO serials VALUES ("{line}", "{ref}", "{dest}", "{start_serial}", "{end_serial}", "{date}")
+        cue.execute(query)
+        if serials_counter % 10 == 0:
+           conn.commit()
+        serial_counter += 1
+    conn.commit()
 
-    df = read_excel(filepath , 1)
-    #sheet one contains failed serial numbers , only colums
+    conn = sqlite3.connect(config.DATABACE_FILE_PATH)
+    cur = conn.cursor()
+
+    cur.execute('DROP TABLE IF EXISTS serials')
+    cur.execute("""CREATE TABLE IF NOT EXISTS people (
+        id INTEGER PRIMARY KEY,
+        ref TEXT,
+        desc TEXT,
+        start serial TEXT,
+        end serial TEXT,
+        date DATE);""")
+    conn.commit()
+
+    df = read_excel(filepath , 1) #sheet one contains failed serial numbers , only colums
+    
     for index , (failed_serial_row) in df.iterrows():
         failed_serial = failed_serial_row[0]
         print(failed_serial)
+
+    cur.execute('DROP TABLE IF EXISTS serials')
+    cur.execute("""CREATE TABLE IF NOT EXISTS people (
+        ref TEXT,
+        desc TEXT,
+        start serial TEXT,
+        end serial TEXT,
+        date DATE);""")
+    conn.commit()
+
+    invalid_counter = 0
+    for index , (failed_serial_row) in df.iterrows():
+        query = f'INSERT INTO serials VALUES ("{faid serial}")
+        cue.execute(query)
+        if invalid_counter % 10 == 0:
+           conn.commit()
+        invalid_counter += 1
+    conn.commit()
+    conn.close()
 
 def check_serial():
     pass
