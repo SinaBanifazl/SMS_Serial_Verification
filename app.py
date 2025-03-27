8fd6b2e46ef76654fb248c7121712d3e746eece0
SinaBanifazl <sinabanifazl81@gmail.com>
2025-03-27 14:15:47 +0330

    video 6 bod ghabli va eshtebah neveshte shod , this is 7 video

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask , jsonify , request
+from pandas import read_excel
 import requests
 import config
 app = Flask(__name__)
@@ -23,7 +24,21 @@ def send_sms(receptor , message):
     r = request.post(url , data)
     print(f"message *{message}* sent status code is {r.status_code}")
 
+def import_databace_from_exel:
+    '''gets an exel file name and imports lookup data (data and failures) from it'''
+    #def contains lookap data in the form of ---
+    df = read_excel(filepasth , 0)
+    for index , (line, ref, dest, start_serial, end_serial, date)
+        print(line, ref, dest, start_serial, end_serial, date)
+
+    df = read_excel(filepath , 1)
+    #sheet one contains failed serial numbers , only colums
+    for index , (failed_serial_row) in df.iterrows():
+        failed_serial = failed_serial_row[0]
+        print(failed_serial)
+
 def check_serial():
+    pass
 
 if __name__ == "__main__":
     app("0.0.0.0" , 5000 , debug=True)
